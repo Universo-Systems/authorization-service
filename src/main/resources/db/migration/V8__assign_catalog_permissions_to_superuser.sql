4b9460540f3649d29bdf4973560ce4137b5f0e35
Christian Alfonso Verduzco Robles <christianverdrobl@gmail.com>
2026-07-10 06:35:17 -0700

    Se agregaron permisos a las migraciones.

---
+++ src/main/resources/db/migration/V8__assign_catalog_permissions_to_superuser.sql
@@ -0,0 +1,4 @@
+INSERT INTO role_permissions (id_role, id_permission)
+SELECT roles.id, permissions.id FROM roles JOIN apps ON apps.id=roles.id_app JOIN permissions ON permissions.id_app=apps.id
+WHERE apps.code='ADMIN_US' AND roles.code='SUPER_USUARIO' AND permissions.code LIKE 'catalog.%'
+AND NOT EXISTS (SELECT 1 FROM role_permissions rp WHERE rp.id_role=roles.id AND rp.id_permission=permissions.id);
